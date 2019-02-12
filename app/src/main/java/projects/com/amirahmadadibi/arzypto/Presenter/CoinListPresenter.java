7b18c04733f0df9dc569db86bd6cb06a9a48927a
AmirahmadAdibi <amirahmadadibi@gmail.com>
2019-02-12 11:15:11 +0330

    fix bitcoin-sv problem

--- app/src/main/java/projects/com/amirahmadadibi/arzypto/Presenter/CoinListPresenter.java
+++ app/src/main/java/projects/com/amirahmadadibi/arzypto/Presenter/CoinListPresenter.java
@@ -86,7 +86,7 @@ public class CoinListPresenter {
                 "eos",
                 "stellar",
                 "litecoin",
-                "bsv",
+                "bitcoin-sv",
                 "tron",
                 "monero",
                 "cardano",
